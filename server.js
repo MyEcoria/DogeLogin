086386b1f84e13c62ada0c00ceee1482794b12de
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2022-12-25 22:46:29 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const WS = require('ws');
+const ReconnectingWebSocket = require('reconnecting-websocket');
+const request = require('request');
+const axios = require('axios');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/data', (req, res) => {
+  const data = {
+    value: Math.random()
+  };
+  res.json(data);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.get('/login', (req, res) => {
+  
+  console.log(req.query);
+  if (req.query['action'] == "create") {
+    const options = {
+    url: 'http://192.168.1.24:7030',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      action: 'account_create',
+      wallet: '2D7A8436F576688215489757D0FFDA7800245857ED9BBDFC49D53C533FBF3B2C'
+    })
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      console.log("error")
+    } else {
+      const body = response.body;
+      const data = JSON.parse(body);
+      res.json(data);
+      console.log(data)
+    }
+  });
+} 
+
+  if (req.query['action'] == "verify") {
+    const add = req.query['account']
+    if (req.query['account'].substring(0, 4) == "xdg_") {
+      
+      const ws = new ReconnectingWebSocket('wss://ws.dogenano.io', [], {
+        WebSocket: WS,
+        connectionTimeout: 1000,  
+        maxRetries: 100000,
+        maxReconnectionDelay: 2000,
+        minReconnectionDelay: 10 // if not set, initial connection will take a few seconds by default
+      });
+
+      // As soon as we connect, subscribe to block confirmations
+      ws.onopen = () => {
+        const confirmation_subscription = {
+          "action": "subscribe", 
+          "topic": "confirmation",
+          "options": { "accounts": [add] }
+        }
+        ws.send(JSON.stringify(confirmation_subscription));
+
+        // Other subscriptions can go here
+      };
+
+      // The node sent us a message
+      ws.onmessage = msg => {
+        data_json = JSON.parse(msg.data);
+        if (data_json.topic === "confirmation") {
+          data = data_json["message"]
+          data = data["account"]
+          if (data != add) {
+            data = { "account": data }
+            console.log(data);
+            res.json(data);
+
+          }
+        }
+
+      };
+
+      setTimeout(function close() {
+        ws.close();
+        res.json("Timeout");
+      }, 60000);
+
+    } else { console.log("Yes"); }
+
+  }
+
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
